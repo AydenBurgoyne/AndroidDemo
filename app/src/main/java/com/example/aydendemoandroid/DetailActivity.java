c3e6c66f8968367eac5ec1a9744b6d0991d8bf4f
pcspecs1 <c3303000@uon.edu.au>
2020-07-16 17:15:49 +1000

    Update DetailActivity.java

--- app/src/main/java/com/example/aydendemoandroid/DetailActivity.java
+++ app/src/main/java/com/example/aydendemoandroid/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.aydendemoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
